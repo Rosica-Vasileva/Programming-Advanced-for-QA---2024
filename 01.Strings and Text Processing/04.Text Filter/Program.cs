a523d095c9d9ad886e40e8b8b84a346f2c1bf04c
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-07-28 01:01:21 +0300

    Add files via upload

--- 01.Strings and Text Processing/04.Text Filter/Program.cs
+++ 01.Strings and Text Processing/04.Text Filter/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//1. входни данни
+
+string[] bannedWords = Console.ReadLine().Split(", ");
+//"Linux, Windows".Split(", ") -> ["Linux", "Windows"]
+
+string text = Console.ReadLine();
+
+//2. обхождаме и заместваме всички забранени думи
+foreach (string banWord in bannedWords)
+{
+    //banWord = "Linux"
+    //replacement = "*****";
+    string replacement = string.Empty; //текста, който ще замести забранената ми дума
+    for (int star = 1; star <= banWord.Length; star++)
+    {
+        replacement += "*";
+    }
+
+    text = text.Replace(banWord, replacement);
+}
+
+//3. отпечатваме финалния текст
+Console.WriteLine(text);
