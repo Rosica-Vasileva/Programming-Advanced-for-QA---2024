e861e2b0339076affbc419d8fba81996659ea8ec
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-07-27 13:47:33 +0300

    Add files via upload

---
+++ 01.Strings and Text Processing/01.Reverse Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿string input = Console.ReadLine();
+
+while (input != "end")
+{
+    //input = "Maria" -> reverse -> "airaM"
+    //обръщам текста
+    string reversedText = ""; //обърнатия текст
+    //всички символи от последния към първия
+    for (int position = input.Length - 1; position >= 0; position--)
+    {
+        char currentSymbol = input[position];
+        reversedText += currentSymbol;
+        //reversedText = reversedText + currentSymbol;
+    }
+
+    //input = "Ivan" -> текст в прав ред
+    //reversedText = "navI" -> текст в обърнат ред
+    Console.WriteLine(input + " = " + reversedText);
+
+
+    input = Console.ReadLine();
+}
