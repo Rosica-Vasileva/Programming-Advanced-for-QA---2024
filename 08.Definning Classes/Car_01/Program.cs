80e76978520242d0738a8c8252f4d6a8e7e2db13
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-07-31 14:19:33 +0300

    Add files via upload

---
+++ 08.Definning Classes/Car_01/Program.cs
@@ -0,0 +1,118 @@
+﻿
+using System;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+
+            Car firstCar = new Car();
+            Car secondCar = new Car(make, model, year);
+            Car thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
+
+            Console.WriteLine(thirdCar.WhoAmI());
+        }
+    }
+
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+        private Engine engine;
+        private Tire[] tires;
+
+        public string Make { get => make; set => make = value; }
+        public string Model { get => model; set => model = value; }
+        public int Year { get => year; set => year = value; }
+        public double FuelQuantity { get => fuelQuantity; set => fuelQuantity = value; }
+        public double FuelConsumption { get => fuelConsumption; set => fuelConsumption = value; }
+        public Engine Engine { get => engine; set => engine = value; }
+        public Tire[] Tires { get => tires; set => tires = value; }
+
+        public void Drive(double distance)
+        {
+            if (FuelQuantity - distance * FuelConsumption > 0)
+            {
+                FuelQuantity -= distance * FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {Make}\nModel: {Model}\nYear: {Year}\nFuel: {FuelQuantity:f2}";
+        }
+
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year) : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires) : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            Engine = engine;
+            Tires = tires;
+        }
+    }
+
+    public class Engine
+    {
+        private int horsePower;
+        private double cubicCapacity;
+
+        public int HorsePower { get => horsePower; set => horsePower = value; }
+        public double CubicCapacity { get => cubicCapacity; set => cubicCapacity = value; }
+
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            HorsePower = horsePower;
+            CubicCapacity = cubicCapacity;
+        }
+    }
+
+    public class Tire
+    {
+        private int year;
+        private double pressure;
+
+        public int Year { get => year; set => year = value; }
+        public double Pressure { get => pressure; set => pressure = value; }
+
+        public Tire(int year, double pressure)
+        {
+            Year = year;
+            Pressure = pressure;
+        }
+    }
+}
